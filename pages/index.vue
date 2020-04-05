1945e3e135e107a0a108f50b3bbb72f40535bedf
fabezio <fabezio@outlook.fr>
2020-04-05 10:27:23 +0200

    mise en place

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.container
+  div
+    h1.title
+      | vitrine_pwa-template
+    h2.subtitle
+      | vitrine
+    .links
+      a.button--green(href='https://nuxtjs.org/', target='_blank')
+        | Documentation
+      a.button--grey(href='https://github.com/nuxt/nuxt.js', target='_blank')
+        | GitHub
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
